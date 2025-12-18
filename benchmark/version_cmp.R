4239fb04890bbe21c1f202fae0a608adf98a9bf1
Valentin Jonquiere <valentin.jonquiere@etu.u-bordeaux.fr>
2025-12-18 19:59:53 +0100

    snapshot slice comparison

--- benchmark/version_cmp.R
+++ benchmark/version_cmp.R
@@ -68,3 +68,20 @@ ggplot(df, aes(x = Nodes, y = Total.Time, color = Snapshot.Interval)) +
   ylab("Total Time (s)")
 
 ggsave(paste(input_file, "_snapshot_interval.png", sep = ""), width = 8, height = 6)
+
+ggplot(df, aes(x = Nodes, y = Total.Time, color = Snapshot.Slices)) +
+  geom_line(aes(group = Snapshot.Slices)) +
+  geom_point(size= 2) +
+  #scale_color_manual(values = c("#D991BA","#58508D", "#7b2d8fff")) +
+  scale_y_log10() +
+  theme(
+    legend.position = c(0.05, 0.95),
+    legend.justification = c("left", "top"),
+    legend.background = element_rect(fill = "white", color = NA),
+    plot.title = element_text(face = "bold"),
+  ) +
+  guides(color = guide_legend(title = "Snapshot slices")) +
+  xlab("Nodes") +
+  ylab("Total Time (s)")
+
+ggsave(paste(input_file, "_snapshot_slices.png", sep = ""), width = 8, height = 6)
