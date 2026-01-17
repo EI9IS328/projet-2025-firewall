c7c97e5fd178598004dfee2cb68f95aec0a248f8
vjonquiere <valentin.jonquiere@etu.u-bordeaux.fr>
2026-01-17 21:01:49 +0100

    fix

--- benchmark/runners/snap_cmp_runner.sh
+++ benchmark/runners/snap_cmp_runner.sh
@@ -25,11 +25,11 @@ mkdir -p "${FOLDER}"
 echo "Running benchmarks..."
 for pb_size in $PROBLEM_SIZE; do
     echo "Problem size: ${pb_size}, Snapshot Frequency: ${SNAPSHOT_FREQUENCY}, Compare with slices: ${COMPARE_WITH_SLICES}"
-    if [ "$COMPARE_WITH_SLICES" == "true" ]; then
+    
       "$EXECUTABLE_PATH" --ex ${pb_size} --ey ${pb_size} --ez ${pb_size} --timemax ${MAX_TIME} --snapshot true --save-interval $SNAPSHOT_FREQUENCY --sismos true --sismos-folder "${FOLDER}" --snap-folder "${FOLDER}" --slices > "${FOLDER}/output_snapshots_${pb_size}_${SNAPSHOT_FREQUENCY}"
-    else
+    
       "$EXECUTABLE_PATH" --ex ${pb_size} --ey ${pb_size} --ez ${pb_size} --timemax ${MAX_TIME} --snapshot true --save-interval $SNAPSHOT_FREQUENCY --sismos true --sismos-folder "${FOLDER}" --snap-folder "${FOLDER}" > "${FOLDER}/output_snapshots_${pb_size}_${SNAPSHOT_FREQUENCY}"
-    fi
+    
     
     "$EXECUTABLE_PATH" --ex ${pb_size} --ey ${pb_size} --ez ${pb_size} --timemax ${MAX_TIME} > "${FOLDER}/output_snapshots_${pb_size}_no_snap"
 done
